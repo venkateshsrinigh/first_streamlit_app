452d4cbe52871124c03875ad24653019f114b3b2
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-01-29 04:36:37 -0700

    Update streamlit_app.py
    updated header and replaced subject with text in code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
-sreamlit.subject('Omega 3 & Blueberry Oatmeal')
-sreamlit.subject('Kale, Spinach & Rocket Smoothie')
-sreamlit.subject('Hard-Boiled Free-Range Egg')
+
+sreamlit.header('Breakfast Menu')
+sreamlit.text('Omega 3 & Blueberry Oatmeal')
+sreamlit.text('Kale, Spinach & Rocket Smoothie')
+sreamlit.text('Hard-Boiled Free-Range Egg')
