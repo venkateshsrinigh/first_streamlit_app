3b0bf480c6df310c09a51bd1a83ea6d5fa96db17
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-01-29 05:07:08 -0700

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
 
-sreamlit.header('Breakfast Favorites')
-sreamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-sreamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-sreamlit.text('🐔 Hard-Boiled Free-Range Egg')
-sreamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
