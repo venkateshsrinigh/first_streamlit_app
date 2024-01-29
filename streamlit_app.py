477ad0de3453df57e3a14c683524676f56427900
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-01-29 04:44:38 -0700

    Update streamlit_app.py
    Changed the header text and added Emojis to the menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
 
-sreamlit.header('Breakfast Menu')
-sreamlit.text('Omega 3 & Blueberry Oatmeal')
-sreamlit.text('Kale, Spinach & Rocket Smoothie')
-sreamlit.text('Hard-Boiled Free-Range Egg')
+sreamlit.header('Breakfast Favorites')
+sreamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+sreamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+sreamlit.text('🐔 Hard-Boiled Free-Range Egg')
+sreamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
