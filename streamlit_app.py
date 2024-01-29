2b321b08584f385e4b61733367e982d183225c0a
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-01-29 04:34:17 -0700

    Update streamlit_app.py
    updated subject

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Heathly Diner')
+sreamlit.subject('Omega 3 & Blueberry Oatmeal')
+sreamlit.subject('Kale, Spinach & Rocket Smoothie')
+sreamlit.subject('Hard-Boiled Free-Range Egg')
