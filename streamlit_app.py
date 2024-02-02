caf20bc2c4bd706587abd4fdd4815c53081fd358
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-02-01 21:26:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,34 +23,3 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-# Create the repeatble code block (called a function)
-def get_fruitycice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
-#New section to display fruitvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice=streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-  else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-# Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
