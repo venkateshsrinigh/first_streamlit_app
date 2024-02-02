4e99c8509a4420e22b1c20dc819d35cd6955741c
venkateshsrinigh <157927918+venkateshsrinigh@users.noreply.github.com>
2024-02-01 21:48:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,9 @@ def get_fruitycice_data(this_fruit_choice):
 #New section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-	streamlit.error("Please select a fruit to get information.")
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
 	else:
 		back_from_function = get_fruityvice_data(fruit_choice)
 		streamlit.dataframe(back_from_function)
